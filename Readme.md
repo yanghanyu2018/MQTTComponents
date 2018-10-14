aefabb416ada0eed155d2e8663b31ab09c47c1ca
Jack Yang <37376711+yanghanyu2018@users.noreply.github.com>
2018-10-14 23:14:25 +0800

    MQTT Server/Client
    以Delphi开发的MQTT 的服务器和客户端。符合MQTT V3.1.

---
+++ Readme.md
@@ -0,0 +1,5 @@
+应用于MQTT Server/Client的Delphi程序。需要安装Overbyte ICS控件。
+
+支持MQTT协议V3.1。 GetRetain特性没有实现。
+
+支持通过Broker进行消息订阅和发布。
---
+++ deltemp.bat
@@ -0,0 +1,15 @@
+del *.ddp
+del *.dcu
+del *.~ddp
+del *.~dpr
+del *.~pas
+del *.~dfm
+del *.$$$
+del *.~???
+del *.bak
+del *.dsk
+del /s *.~???
+del /Q output\*.*
+del *.map
+del *_EventLog.txt
+del *.drc
